542a99018c719fbc9a105bfdccc02b4f206ce229
Laily97 <79189049+Laily97@users.noreply.github.com>
2022-04-01 15:36:03 +0800

    test deploy

---
+++ src/Home.js
@@ -0,0 +1,41 @@
+import API from "./config/instance";
+
+class Home extends React.Component {
+    defaultState = {
+      owners: [],
+    };
+    constructor() {
+      super();
+      this.listOwner = this.listOwner.bind(this);
+      this.state = this.defaultState;
+    }
+  
+    componentDidMount(){
+      this.listOwner();
+    }
+  
+    async listOwner() {
+      const { status, data } = await API.get("/ltaodataservice/TaxiStands");
+      if (status === 200) {
+        this.setState((state) => {
+          state.owners = data.value;
+          return state;
+        });
+      }
+    }
+  
+    render() {
+      return (
+        <>
+          
+          <ul>
+            {this.state.owners.map((o) => {
+              return <li>{o.TaxiCode}</li>;
+            })}
+          </ul>
+        </>
+      );
+    }
+  }
+  
+  export default Home;
